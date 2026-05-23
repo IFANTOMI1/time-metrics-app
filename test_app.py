d63b37a60d4a5ec777f14cb17f572b11ac06fa7f
IFANTOMI1 <ifantomi1@yandex.ru>
2026-05-23 13:09:38 +0300

    Added a new test for route metrics

--- test_app.py
+++ test_app.py
@@ -6,3 +6,10 @@ def test_time_route():
     response = client.get('/time')
     data = response.get_json()
     assert data['time'] > 0
+
+def test_metrics_route():
+    client = app.test_client()
+    client.get('/time')
+    response = client.get('/metrics')
+    data = response.get_json()
+    assert data['count'] == 1
